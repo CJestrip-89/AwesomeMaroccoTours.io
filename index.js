dc24548ed7d3657d293c6d1c68b5c30918ad8cee
chris <chrisestriplet27@gmail.com>
2023-04-11 22:22:12 -0400

    modified navbar styling on scroll

--- index.js
+++ index.js
@@ -8,13 +8,33 @@ document.querySelector("#nav-close").onclick = () => {
   navbar.classList.remove("active");
 };
 
-// Header funtionality
+// Header funtionalities
 const headerEl = document.querySelector(".header");
 window.addEventListener("scroll", () => {
   if (window.scrollY > 0) {
     headerEl.classList.add("header-scrolled");
   } else if (window.scrollY <= 0) {
     headerEl.classList.remove("header-scrolled");
+  }
+});
+
+let textElement = document.querySelector(".text");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    textElement.classList.add("scroll-text");
+  } else if (window.scrollY <= 0) {
+    textElement.classList.remove("scroll-text");
+  }
+});
+
+let headerLogo = document.querySelector(".logo-img");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    headerLogo.classList.add("logo-img__scroll");
+  } else if (window.scrollY <= 0) {
+    headerLogo.classList.remove("logo-img__scroll");
   }
 });
 
